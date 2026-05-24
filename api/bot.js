b43ad280d3ec32de35343893e9aebb3de09241ec
BarboSSA <htutkhaungkoko336@gmail.com>
2026-05-24 14:24:09 +0630

    6

--- api/bot.js
+++ api/bot.js
@@ -121,4 +121,4 @@ bot.action(/reject_.+/, async (ctx) => {
 module.exports = async (req, res) => {
     try { await bot.handleUpdate(req.body); res.status(200).send('OK'); }
     catch (err) { console.error(err); res.status(500).send('Internal Server Error'); }
-};
+}; 
